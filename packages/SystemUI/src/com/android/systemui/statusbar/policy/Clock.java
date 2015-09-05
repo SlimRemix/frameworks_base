dac786b3ec979b48fd1204bd361d586b2922a568
Janson Kang <temasek71@gmail.com>
2015-09-05 01:17:19 -0700

    FWB: Statusbar clock font size (1/2)
    Signed-off-by: Janson Kang <temasek71@gmail.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
@@ -99,6 +99,7 @@ public class Clock extends TextView implements DemoMode {
     protected boolean mShowClock;
     private int mClockAndDateWidth;
     protected boolean mShowClockSeconds = false;
+    private int mClockFontSize = 14;
 
     private int mAmPmStyle;
 
@@ -132,6 +133,9 @@ public class Clock extends TextView implements DemoMode {
                     this, UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.STATUSBAR_CLOCK_FONT_STYLE),
+                    false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.STATUSBAR_CLOCK_FONT_SIZE),
                     false, this, UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System
                     .getUriFor(Settings.System.STATUSBAR_CLOCK_DATE_POSITION), false,
@@ -400,6 +404,9 @@ public class Clock extends TextView implements DemoMode {
         mClockFontStyle = Settings.System.getIntForUser(resolver,
                 Settings.System.STATUSBAR_CLOCK_FONT_STYLE, FONT_NORMAL,
                 UserHandle.USER_CURRENT);
+        mClockFontSize = Settings.System.getIntForUser(resolver,
+                Settings.System.STATUSBAR_CLOCK_FONT_SIZE, 14,
+                UserHandle.USER_CURRENT);
 
         int defaultColor = getResources().getColor(R.color.status_bar_clock_color);
         int clockColor = Settings.System.getIntForUser(resolver,
@@ -435,6 +442,7 @@ public class Clock extends TextView implements DemoMode {
         if (mAttached) {
             getFontStyle(mClockFontStyle);
             setTextColor(clockColor);
+            setTextSize(mClockFontSize);
             updateClockVisibility();
             updateClock();
         }
@@ -458,23 +466,23 @@ public class Clock extends TextView implements DemoMode {
     public void getFontStyle(int font) {
         switch (font) {
             case FONT_BOLD:
-                mClockView.setTypeface(Typeface.create("sans-serif", Typeface.BOLD));
+                setTypeface(Typeface.create("sans-serif", Typeface.BOLD));
                 break;
             case FONT_BOLD_ITALIC:
-                mClockView.setTypeface(Typeface.create("sans-serif", Typeface.BOLD_ITALIC));
+                setTypeface(Typeface.create("sans-serif", Typeface.BOLD_ITALIC));
                 break;
             case FONT_CONDENSED:
-                mClockView.setTypeface(Typeface.create("sans-serif-condensed", Typeface.NORMAL));
+                setTypeface(Typeface.create("sans-serif-condensed", Typeface.NORMAL));
                 break;
             case FONT_LIGHT:
-                mClockView.setTypeface(Typeface.create("sans-serif-light", Typeface.NORMAL));
+                setTypeface(Typeface.create("sans-serif-light", Typeface.NORMAL));
                 break;
             case FONT_LIGHT_ITALIC:
-                mClockView.setTypeface(Typeface.create("sans-serif-light", Typeface.ITALIC));
+                setTypeface(Typeface.create("sans-serif-light", Typeface.ITALIC));
                 break;
             case FONT_NORMAL:
             default:
-                mClockView.setTypeface(Typeface.create("sans-serif", Typeface.NORMAL));
+                setTypeface(Typeface.create("sans-serif", Typeface.NORMAL));
                 break;
         }
     }
