c92f3e4837051c2bfc1ffb9e484dbdb972574ffa
srisurya95 <srisurya95@gmail.com>
2015-09-18 01:36:32 +0000

    Revert "telephony: Handle NPE in setDataEnabledUsingSubId()"
    This reverts commit b421cefc6fb54e89393233ec831a7eb0fd7dcaf1.

--- telephony/java/android/telephony/TelephonyManager.java
+++ telephony/java/android/telephony/TelephonyManager.java
@@ -3845,8 +3845,6 @@ public class TelephonyManager {
             getITelephony().setDataEnabled(subId, enable);
         } catch (RemoteException e) {
             Log.e(TAG, "Error calling setDataEnabled", e);
-        } catch (NullPointerException npe) {
-            Log.e(TAG, "Error calling setDataEnabled", npe);
         }
     }
 
